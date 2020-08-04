4257021d333dec76fbf390a527c7d544306d3ddc
th1095l <67662582+th1095l@users.noreply.github.com>
2020-08-05 10:35:48 +1200

    Update application.py
    Lots of work done in creating the questions and answers and cycling through it.
    Need to next place dictionary outside of file in another file to make it smaller.

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from string import punctuation
+from random import randint
 LARGE_FONT = ("Verdana", 12)
 
 class ApplicationFramework(tk.Tk):
@@ -71,6 +72,22 @@ class QuestionPage(tk.Frame):
                   "b": "somethinga 2",
                   "c": "somethinga 3",
                   "d": "somethinga 4"},
+             "correct_answer": "c"},
+        "question_3":
+            {"question": "test question 3",
+             "answers":
+                 {"a": "somethingav",
+                  "b": "somethingav 2",
+                  "c": "somethingav 3",
+                  "d": "somethingav 4"},
+             "correct_answer": "d"},
+        "question_4":
+            {"question": "test question 4",
+             "answers":
+                 {"a": "somethingavv",
+                  "b": "somethingavv 2",
+                  "c": "somethingavv 3",
+                  "d": "somethingavv 4"},
              "correct_answer": "c"}
         }
     
@@ -78,23 +95,29 @@ class QuestionPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.question_counter = 0
         question_frame = tk.Frame(self, relief="sunken", bg="black")
         question_frame.pack(fill="both", padx=10, pady=5)
         test_question = tk.Label(question_frame, text="Question frame")
-        test_question.pack()
+        test_question.grid(row=0, column=1)
         answer_frame = tk.Frame(self, relief="sunken", bg="red")
         answer_frame.pack(fill="both", padx=10, pady=5)
         test_answer = tk.Label(answer_frame, text="Answer frame")
         test_answer.pack()
-        question_label_array = []
-        for F in biology_questions:
-            question_label_array.append(tk.Label(question_frame, text=biology_questions[F]["question"]))
-            print (question_label_array)
-        question_label_array[0].pack()
-        iterator = 1
-        text_next_button = tk.Button(self, text="next_question", command=lambda: question_label_array[iterator].pack())
-        text_next_button.pack()
-        
+        self.question_label_array = []
+        for F, i in zip((biology_questions), (range(len(biology_questions)))):
+            question_label = tk.Label(question_frame, text=biology_questions[F]["question"])
+            self.question_label_array.append(question_label)
+            self.question_label_array[i].grid(row=0, column=0, sticky="nsew")
+        self.iterate_question(0)
+        next_question_button = tk.Button(self, text="next", command=lambda: self.iterate_question(self.question_counter))
+        next_question_button.pack()
+
+
+    def iterate_question(self, question):
+        current_question = self.question_label_array[question]
+        self.question_counter += 1
+        current_question.tkraise()
         
         
         
