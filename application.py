5d0b658b220631b7ecdb663ca760b8dd7db84387
th1095l <67662582+th1095l@users.noreply.github.com>
2020-08-06 12:35:42 +1200

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from string import punctuation
 from random import randint
+from questions import *
 LARGE_FONT = ("Verdana", 12)
 
 class ApplicationFramework(tk.Tk):
@@ -54,42 +55,6 @@ class OpeningPage(tk.Frame):
         next_button.pack()
 
 class QuestionPage(tk.Frame):
-
-    global biology_questions
-    biology_questions = {
-        "question_1":
-            {"question": "test question 1",
-             "answers":
-                 {"a": "something",
-                  "b": "something 2",
-                  "c": "something 3",
-                  "d": "something 4"},
-             "correct_answer": "b"},
-        "question_2":
-            {"question": "test question 2",
-             "answers":
-                 {"a": "somethinga",
-                  "b": "somethinga 2",
-                  "c": "somethinga 3",
-                  "d": "somethinga 4"},
-             "correct_answer": "c"},
-        "question_3":
-            {"question": "test question 3",
-             "answers":
-                 {"a": "somethingav",
-                  "b": "somethingav 2",
-                  "c": "somethingav 3",
-                  "d": "somethingav 4"},
-             "correct_answer": "d"},
-        "question_4":
-            {"question": "test question 4",
-             "answers":
-                 {"a": "somethingavv",
-                  "b": "somethingavv 2",
-                  "c": "somethingavv 3",
-                  "d": "somethingavv 4"},
-             "correct_answer": "c"}
-        }
     
 
     def __init__(self, parent, controller):
