ade13b0ac8797f438e98d0a6006649aa8927c45a
th1095l <67662582+th1095l@users.noreply.github.com>
2020-08-06 12:36:10 +1200

    Question Storage

---
+++ questions.py
@@ -0,0 +1,34 @@
+biology_questions = {
+        "question_1":
+            {"question": "test question 1",
+             "answers":
+                 {"a": "something",
+                  "b": "something 2",
+                  "c": "something 3",
+                  "d": "something 4"},
+             "correct_answer": "b"},
+        "question_2":
+            {"question": "test question 2",
+             "answers":
+                 {"a": "somethinga",
+                  "b": "somethinga 2",
+                  "c": "somethinga 3",
+                  "d": "somethinga 4"},
+             "correct_answer": "c"},
+        "question_3":
+            {"question": "test question 3",
+             "answers":
+                 {"a": "somethingav",
+                  "b": "somethingav 2",
+                  "c": "somethingav 3",
+                  "d": "somethingav 4"},
+             "correct_answer": "d"},
+        "question_4":
+            {"question": "test question 4",
+             "answers":
+                 {"a": "somethingavv",
+                  "b": "somethingavv 2",
+                  "c": "somethingavv 3",
+                  "d": "somethingavv 4"},
+             "correct_answer": "c"}
+        }
