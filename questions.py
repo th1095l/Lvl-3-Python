b85d3565bfc77ad11e3b5512fdd18f48ab724c7e
th1095l <67662582+th1095l@users.noreply.github.com>
2020-09-02 10:09:08 +1200

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,34 +1,58 @@
 biology_questions = {
-        "question_1":
+        0:
             {"question": "test question 1",
              "answers":
-                 {"a": "something",
-                  "b": "something 2",
-                  "c": "something 3",
-                  "d": "something 4"},
+                 {"a": "a",
+                  "b": "correct b",
+                  "c": "c",
+                  "d": "d"},
              "correct_answer": "b"},
-        "question_2":
+        1:
             {"question": "test question 2",
              "answers":
-                 {"a": "somethinga",
-                  "b": "somethinga 2",
-                  "c": "somethinga 3",
-                  "d": "somethinga 4"},
+                 {"a": "2 a",
+                  "b": "2 b",
+                  "c": " correct 2 c",
+                  "d": "2 d"},
              "correct_answer": "c"},
-        "question_3":
+        2:
             {"question": "test question 3",
              "answers":
-                 {"a": "somethingav",
-                  "b": "somethingav 2",
-                  "c": "somethingav 3",
-                  "d": "somethingav 4"},
+                 {"a": "3 a",
+                  "b": "3 b",
+                  "c": "3 c",
+                  "d": "correct 3 d"},
              "correct_answer": "d"},
-        "question_4":
+        3:
             {"question": "test question 4",
              "answers":
-                 {"a": "somethingavv",
-                  "b": "somethingavv 2",
-                  "c": "somethingavv 3",
-                  "d": "somethingavv 4"},
+                 {"a": "4 a",
+                  "b": "4 b",
+                  "c": "correct 4 c",
+                  "d": "4 d"},
+             "correct_answer": "c"},
+        4:
+            {"question": "test question 5",
+             "answers":
+                 {"a": "5 a",
+                  "b": "5 b",
+                  "c": "correct 5 c",
+                  "d": "5 d"},
+             "correct_answer": "c"},
+        5:
+            {"question": "test question 6",
+             "answers":
+                 {"a": "6 a",
+                  "b": "6 b",
+                  "c": "correct 6 c",
+                  "d": "6 d"},
+             "correct_answer": "c"},
+        6:
+            {"question": "test question 7",
+             "answers":
+                 {"a": "7 a",
+                  "b": "7 b",
+                  "c": "correct 7 c",
+                  "d": "7 d"},
              "correct_answer": "c"}
         }
